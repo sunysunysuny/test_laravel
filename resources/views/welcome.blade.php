aa3a39ceca123b96f616144fe5bea8b39fb357c6
sunny-figma <sunnyfigma@gmail.com>
2020-06-29 14:08:37 +0530

    little change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 12345
                 </div>
 
                 <div class="links">
